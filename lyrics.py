af0c39b6ac842e41753234c94d07771f2c63fcee
ewan_galin <ewan.galin@sogilis.com>
2023-03-16 14:30:54 +0100

    put python test ok

--- lyrics.py
+++ lyrics.py
@@ -1,4 +1,4 @@
 def sing() -> list[str]:
     # To create a string list in python use ["first element", "second element"]
-    raise Exception("Not implemented :(")
+    return ["When I find myself in times of trouble, Mother Mary comes to me"]
     
