5d7cfde6d58b65d489ccd4a3fe3a572da7dc4e88
Claire <claire.rosset@unity-sc.com>
2023-03-16 11:56:41 +0100

    refacto

--- lyrics.py
+++ lyrics.py
@@ -1,19 +1,22 @@
 def sing() -> list[str]:
-    return ["When I find myself in times of trouble, Mother Mary comes to me",
-    "Speaking words of wisdom, let it be",
-    "And in my hour of darkness she is standing right in front of me",
-    "Speaking words of wisdom, let it be",
+    return [
+        "When I find myself in times of trouble, Mother Mary comes to me",
+        "Speaking words of wisdom, let it be",
+        "And in my hour of darkness she is standing right in front of me",
+        "Speaking words of wisdom, let it be",
+    ]
 
-    "Let it be, let it be, let it be, let it be",
-    "Whisper words of wisdom, let it be",]
 
 def first_verse() -> list[str]:
-    return ["When I find myself in times of trouble, Mother Mary comes to me",
-            "Speaking words of wisdom, let it be",
-            "And in my hour of darkness she is standing right in front of me",
-            "Speaking words of wisdom, let it be", ]
+    return [
+        "When I find myself in times of trouble, Mother Mary comes to me",
+        "Speaking words of wisdom, let it be",
+        "And in my hour of darkness she is standing right in front of me",
+    ]
 
 
 def second_verse() -> list[str]:
-    return ["Let it be, let it be, let it be, let it be",
-            "Whisper words of wisdom, let it be",]
+    return [
+        "Let it be, let it be, let it be, let it be",
+        "Whisper words of wisdom, let it be",
+    ]
