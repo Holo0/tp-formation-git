c8a9c7ba06f7e8b7470e5996761bb8b385c76775
ewan_galin <ewan.galin@sogilis.com>
2023-03-16 15:49:42 +0100

    sing

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,10 @@
 def first_verse() -> list[str]:
-    raise Exception("firt_verse not implemented :(")
+    return [
+            "When I find myself in times of trouble, Mother Mary comes to me",
+            "Speaking words of wisdom, let it be",
+            "And in my hour of darkness she is standing right in front of me",
+            "Speaking words of wisdom, let it be",
+        ]
 
 def second_verse() -> list[str]:
     return [
